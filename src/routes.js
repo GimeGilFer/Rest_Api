ba0550a1f57b10f02d6b540dbb8c5710194d6776
Gimena Gil Fernández <104610596+GimeGilFer@users.noreply.github.com>
2023-10-28 18:12:08 -0300

    Eliminación de libros por ID e ISBN

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,8 @@ import { libro } from './controller.js'
 export const router = Router() 
 
 router.get('/libros', libro.getAll);
-router.post('/libro', libro.add);
-router.delete('/libro', libro.delete);
-router.put ('/libro', libro.update);
 router.get('/libro', libro.getOne);
+router.post('/libro', libro.add);
+router.delete('/libroISBN',libro.deleteISBN);
+router.delete('/libroID',libro.deleteID);
+router.put('/libro',libro.update);
